046d60a0ba6f161d6b40b197f1c0c5dccd5fc20b
Noah_Yang <ykd1217@gmail.com>
2018-10-15 15:48:21 +0900

    Update env

--- packages/webpack/utils/env.js
+++ packages/webpack/utils/env.js
@@ -4,22 +4,23 @@
 const dotenv = require('dotenv');
 
 // Load environment variables from .env file
-const env = dotenv.config();
+dotenv.config();
+
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const configValue = {
   // base env
   base: {
-    CDN_URL: env.parsed.CDN_URL || '',
-    FACEBOOK_APP_ID: env.parsed.FACEBOOK_APP_ID || '',
-    FACEBOOK_PIXEL_ID: env.parsed.FACEBOOK_PIXEL_ID || '',
-    GA_TRACKING_ID: env.parsed.GA_TRACKING_ID || '',
-    NAVER_APP_ID: env.parsed.NAVER_APP_ID || '',
+    CDN_URL: process.env.CDN_URL || '',
+    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || '',
+    FACEBOOK_PIXEL_ID: process.env.FACEBOOK_PIXEL_ID || '',
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID || '',
+    NAVER_APP_ID: process.env.NAVER_APP_ID || '',
     NODE_ENV
   },
   production: {},
   development: {
     HOST: 'localhost',
-    PORT: env.parsed.PORT || 3000
+    PORT: process.env.PORT || 3000
   },
   test: {}
 };
